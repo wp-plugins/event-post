013bf9935c0dcc755d78974521c8c5a126f7f1e5
bastho <bastho@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-16 15:05:08 +0000

    update version
    git-svn-id: https://plugins.svn.wordpress.org/event-post/trunk@1008642 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- eventpost.php
+++ eventpost.php
@@ -3,7 +3,7 @@
   Plugin Name: Event Post
   Plugin URI: http://ecolosites.eelv.fr/articles-evenement-eventpost/
   Description: Add calendar and/or geolocation metadata on posts
-  Version: 2.8.12
+  Version: 3.0.0
   Author: bastho, n4thaniel, ecolosites // EÉLV
   Author URI: http://ecolosites.eelv.fr/
   License: GPLv2
